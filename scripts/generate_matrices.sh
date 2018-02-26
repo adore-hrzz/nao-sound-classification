88e213c7e1d5a77450037154ceba6240fb25e6ea
fpetric <f5r1c.1m0@gmail.com>
2018-02-26 12:47:03 +0100

    Fixed matrix generation

--- scripts/generate_matrices.sh
+++ scripts/generate_matrices.sh
@@ -2,7 +2,7 @@
 
 for i in {1..100} 
 do
-    split_train_test_data.py ./Resources/output.txt ./Resources/class_info.txt
+    ./split_train_test_data.py ./Resources/output.txt ./Resources/class_info.txt
     ./Learner SC_train.config
     ./folderClassify SC_train.config
     mv results.csv ./Resources/Train/results_train$i.csv
